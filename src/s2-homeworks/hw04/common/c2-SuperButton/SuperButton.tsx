333c762bea6499a05da78640dc23650f0042f208
Vr930 <Vr930@mail.ru>
2023-03-04 14:05:44 +0300

    fix chekbox

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -21,7 +21,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         + (disabled ? ' ' + s.disabled :
             xType === 'red' ? ' ' + s.red :
                 xType === 'secondary' ? ' ' + s.secondary :
-                    "") + (className ? ' ' + className : '')
+                    ' ' + s.default) + (className ? ' ' + className : '')
 
     // const finalClassName = s.button
     // + (disabled
